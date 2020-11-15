e02777790ad7f240c34d064f2811fdda062da592
J_Cat <joe@cataford.com>
2020-11-15 10:16:28 -0500

    Updated error in new fonts.

--- dist/fonts.js
+++ dist/fonts.js
@@ -5,9 +5,9 @@ var Font;
 (function (Font) {
     Font[Font["UbuntuMono_8ptFontInfo"] = 0] = "UbuntuMono_8ptFontInfo";
     Font[Font["UbuntuMono_10ptFontInfo"] = 1] = "UbuntuMono_10ptFontInfo";
-    Font[Font["UbuntuMono_12ptFontInfo"] = 1] = "UbuntuMono_12ptFontInfo";
-    Font[Font["UbuntuMono_16ptFontInfo"] = 2] = "UbuntuMono_16ptFontInfo";
-    Font[Font["UbuntuMono_24ptFontInfo"] = 3] = "UbuntuMono_24ptFontInfo";
-    Font[Font["UbuntuMono_48ptFontInfo"] = 4] = "UbuntuMono_48ptFontInfo";
+    Font[Font["UbuntuMono_12ptFontInfo"] = 2] = "UbuntuMono_12ptFontInfo";
+    Font[Font["UbuntuMono_16ptFontInfo"] = 3] = "UbuntuMono_16ptFontInfo";
+    Font[Font["UbuntuMono_24ptFontInfo"] = 4] = "UbuntuMono_24ptFontInfo";
+    Font[Font["UbuntuMono_48ptFontInfo"] = 5] = "UbuntuMono_48ptFontInfo";
 })(Font = exports.Font || (exports.Font = {}));
 //# sourceMappingURL=fonts.js.map
